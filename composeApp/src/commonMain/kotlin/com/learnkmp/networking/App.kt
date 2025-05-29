bfbcadef8dc79827c89407be85876337023c2880
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2025-05-29 19:43:37 +0700

    Solution implemented

--- composeApp/src/commonMain/kotlin/com/learnkmp/networking/App.kt
+++ composeApp/src/commonMain/kotlin/com/learnkmp/networking/App.kt
@@ -1,17 +1,34 @@
 package com.learnkmp.networking
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.request.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
-import io.ktor.http.*
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -24,6 +41,7 @@ fun App() {
 }
 
 @Composable
+@Preview
 fun MessageBoardScreen() {
     val client = remember { HttpClient(CIO) }
     var message by remember { mutableStateOf("") }
@@ -60,7 +78,6 @@ fun MessageBoardScreen() {
             modifier = Modifier.fillMaxWidth()
         )
 
-        Text("Blob URL = $blobUrl")
         Spacer(modifier = Modifier.height(16.dp))
 
         Row(
@@ -69,10 +86,26 @@ fun MessageBoardScreen() {
         ) {
             Button(onClick = {
                 performApiOperation {
+
+                    //TODO
+                    // 1. In order for us to work with something
+                    // we first need to post a value
+                    // Your task is to build a POST with a json content type
+                    // In the body you should put the `createJsonBody()` function that
+                    // we created above which will represent the json we're trying to send
+
                     val response = client.post("https://www.jsonblob.com/api/jsonBlob") {
                         contentType(ContentType.Application.Json)
                         setBody(createJsonBody())
                     }
+                    // we have to replace further calls with https for this to work
+                    //TODO
+                    // 2. In the header of our response we get a blob URL of our JSON
+                    // We need to save it in order to perform further operations
+                    // Your job is to retrieve this from the header and save it into the
+                    // blobUrl variable that we have above.
+                    // Also, you will need to replace the "http" part in the blob URL with "https"
+
                     blobUrl = response.headers["Location"]?.replace("http", "https")
                     statusMessage = "✅ POST success! Blob created with URL = $blobUrl"
                 }
@@ -82,12 +115,18 @@ fun MessageBoardScreen() {
 
             Button(onClick = {
                 performApiOperation(requiresBlobUrl = true) {
+                    //TODO
+                    // 3. Build a PUT operation that takes our blobUrl variable
+                    // Make sure to set the contentType as JSON
+                    // and the body should be the `createJsonBody()` function that we have above.
+
                     val response = client.put(blobUrl!!) {
                         contentType(ContentType.Application.Json)
                         setBody(createJsonBody())
                     }
-                    statusMessage =
-                        "✅ PUT success! Status = ${response.status}. Response: ${response.bodyAsText()}"
+
+//                    statusMessage =
+//                        "✅ PUT success! Status = ${response.status}. Response: ${response.bodyAsText()}"
                 }
             }) {
                 Text("PUT")
@@ -102,6 +141,11 @@ fun MessageBoardScreen() {
         ) {
             Button(onClick = {
                 performApiOperation(requiresBlobUrl = true) {
+                    // TODO
+                    //  4. Perform a GET operation
+                    //  We're not aiming to parse the response as of now, but simply print what it returns
+                    //  Hint: Use the `bodyAsText()` to get the response body as text.
+
                     val response = client.get(blobUrl!!).bodyAsText()
                     statusMessage = "✅ GET success: $response"
                 }
@@ -111,6 +155,9 @@ fun MessageBoardScreen() {
 
             Button(onClick = {
                 performApiOperation(requiresBlobUrl = true) {
+                    // TODO
+                    //  5. Perform a DELETE operation by supplying our blobUrl as the urlString
+
                     client.delete(blobUrl!!)
                     statusMessage = "✅ DELETE success. Blob deleted."
                     blobUrl = null // Reset blob URL after deletion
