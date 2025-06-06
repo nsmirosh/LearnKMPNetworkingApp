1120dc88d2fdfad9c9d3693a0ca87669a03346cf
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2025-06-06 15:12:50 +0700

    Prepared the code

--- composeApp/src/commonMain/kotlin/com/learnkmp/networking/App.kt
+++ composeApp/src/commonMain/kotlin/com/learnkmp/networking/App.kt
@@ -96,79 +96,36 @@ fun MessageBoardScreen() {
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             ApiButton("GET", requiresBlobUrl = true) {
-                // TODO
-                //  Perform a GET operation here and save the response into a local `response` variable
-                //  We're not aiming to parse the response as of now, but will simply print what it returns
-                //  Hint: Use the `bodyAsText()` to get the response body as text.
-
                 blobUrl?.let { url ->
                     val response = client.get(url).bodyAsText()
-
-                    // TODO
-                    //  Uncomment the code below after you implemented the GET request above
 
                     statusMessage = "✅ GET success: $response"
                 }
             }
 
             ApiButton("POST") {
-                //TODO
-                // In order for us to work with something
-                // We first need to post a value
-                // Your task is to make a POST request to https://www.jsonblob.com/api/jsonBlob
-                // The content type should be JSON
-                // In the body you should put the `createJsonBody()` function that
-                // we created above which will take what we have in our TextField
-                // and package it into the JSON format
-
                 val response = client.post("https://www.jsonblob.com/api/jsonBlob") {
                     contentType(ContentType.Application.Json)
                     setBody(createJsonBody())
                 }
 
-                //TODO
-                // After you have implemented the call above and got the response
-                // in the header of your response you will get a URL of where our JSON will be stored
-                // We need to save this URL in order to perform further operations
-                // Your job is to retrieve this from the header and save it into the
-                // `blobUrl` variable that we have at the start of MessageBoardScreen above.
-                // IMPORTANT(!) - once you retrieve the URL from the header
-                // Before storing it you will need to replace "http" with "https" for redirection.
-
                 blobUrl = response.headers["Location"]?.replace("http", "https")
 
-
-                //TODO uncomment this once you implement the above functionality
                 statusMessage = "✅ POST success! Blob created with URL = $blobUrl"
             }
 
             ApiButton("PUT", requiresBlobUrl = true) {
-                //TODO
-                // Build a PUT request to the URL that we have saved in the `blobUrl` variable
-                // Make sure to set the contentType as JSON
-                // Use the `createJsonBody()` function that we have above for the body.
-                // Save the response into a local `response` variable
-
                 val response = client.put(blobUrl!!) {
                     contentType(ContentType.Application.Json)
                     setBody(createJsonBody())
                 }
-
-                //TODO uncomment this after you get the response
 
                 statusMessage =
                     "✅ PUT success! Status = ${response.status}. Response: ${response.bodyAsText()}"
             }
 
             ApiButton("DELETE", requiresBlobUrl = true) {
-                // TODO
-                //  Perform a DELETE with our `blobUrl` as path
-                //  We don't need any response from the delete operation
-
                 client.delete(blobUrl!!)
-
-                // TODO
-                //  Uncomment this after you implement the delete operation above
 
                 statusMessage = "✅ DELETE success. Blob deleted."
                 blobUrl = null // Reset blob URL after deletion
