dcfd3148da77ee2cdbfaa8f7372cb1dbb7782be1
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2025-09-12 17:12:51 +0700

    Logging solution implemented

--- composeApp/src/androidMain/kotlin/com/learnkmp/networking/helpers/NetworkHelper.android.kt
+++ composeApp/src/androidMain/kotlin/com/learnkmp/networking/helpers/NetworkHelper.android.kt
@@ -1,23 +1,38 @@
 package com.learnkmp.networking.helpers
 
+import com.learnkmp.networking.models.Note
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpSend
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.plugin
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.contextual
-import kotlin.time.Instant
-import kotlin.time.ExperimentalTime
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 
-@OptIn(ExperimentalTime::class)
-actual fun createPlatformHttpClient(): HttpClient = HttpClient(OkHttp) {
-    install(ContentNegotiation) {
-        json()
+actual fun createPlatformHttpClient(): HttpClient {
+    val client = HttpClient(OkHttp) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
     }
+
+    client.plugin(HttpSend).intercept { request ->
+        // Simple request log
+        println("[HTTP] -> ${request.method.value} ${request.url}")
+        println("[HTTP] -> ${request.body}")
+
+        val call = execute(request)
+
+        // Simple response log
+        println("[HTTP] <- ${call.response.status.value} ${call.request.url}")
+        val note = call.response.body<Note>()
+        println("[HTTP] <- $note")
+        call
+    }
+    return client
 }
--- composeApp/src/iosMain/kotlin/com/learnkmp/networking/helpers/NetworkHelper.ios.kt
+++ composeApp/src/iosMain/kotlin/com/learnkmp/networking/helpers/NetworkHelper.ios.kt
@@ -1,8 +1,38 @@
 package com.learnkmp.networking.helpers
 
+import com.learnkmp.networking.models.Note
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.darwin.Darwin
-import kotlin.time.ExperimentalTime
+import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.plugin
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
-@OptIn(ExperimentalTime::class)
-actual fun createPlatformHttpClient(): HttpClient = HttpClient(Darwin)
+actual fun createPlatformHttpClient(): HttpClient {
+    val client = HttpClient(Darwin) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    client.plugin(HttpSend).intercept { request ->
+        // Simple request log
+        println("[HTTP] -> ${request.method.value} ${request.url}")
+        println("[HTTP] -> ${request.body}")
+
+        val call = execute(request)
+
+        // Simple response log
+        println("[HTTP] <- ${call.response.status.value} ${call.request.url}")
+        val note = call.response.body<Note>()
+        println("[HTTP] <- $note")
+        call
+    }
+    return client
+}
