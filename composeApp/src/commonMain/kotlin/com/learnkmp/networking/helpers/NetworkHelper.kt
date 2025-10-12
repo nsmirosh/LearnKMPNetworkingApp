9d255c53414eb41c4c021172a45cc2e2b9b567af
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2025-10-12 19:02:02 +0700

    some clean up

--- composeApp/src/commonMain/kotlin/com/learnkmp/networking/helpers/NetworkHelper.kt
+++ composeApp/src/commonMain/kotlin/com/learnkmp/networking/helpers/NetworkHelper.kt
@@ -1,16 +1,9 @@
 package com.learnkmp.networking.helpers
 
 import io.ktor.client.HttpClient
-import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.cookies.AcceptAllCookiesStorage
-import io.ktor.client.plugins.cookies.HttpCookies
-import io.ktor.client.plugins.logging.DEFAULT
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.request.header
-import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 expect fun createPlatformHttpClient(): HttpClient
 
