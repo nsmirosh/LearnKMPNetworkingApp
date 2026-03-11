70689e7c74509d7976697172e666d1249494d8d2
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-03-11 20:09:31 +0700

    Finished with the first call code

--- composeApp/src/commonMain/kotlin/com/learnkmp/networking/App.kt
+++ composeApp/src/commonMain/kotlin/com/learnkmp/networking/App.kt
@@ -1,22 +1,21 @@
 package com.learnkmp.networking
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kmpnetworkingapp.composeapp.generated.resources.Res
-import kmpnetworkingapp.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
@@ -36,5 +35,10 @@ fun App() {
 }
 
 private fun buildKtor() {
-
+    val client = HttpClient(CIO)
+    val coroutineScope = CoroutineScope(Dispatchers.Default)
+    coroutineScope.launch {
+        val response = client.get("http://example.com/")
+        println("response.status = ${response.status}")
+    }
 }
